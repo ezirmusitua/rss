1ee340d4b125e2841c533a11ed2bcfa3f42328b8
ezirmusitua <jferroal@gmail.com>
2017-12-08 08:29:57 +0800

    Lint
    Took 5 hours 27 minutes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,38 +1,38 @@
 import * as React from 'react';
 import './App.css';
-import {ApolloClient, ApolloProvider, gql, graphql, createNetworkInterface} from 'react-apollo';
-
+import { ApolloClient, ApolloProvider, gql, graphql, createNetworkInterface, QueryProps } from 'react-apollo';
 
 const client = new ApolloClient({
     networkInterface: createNetworkInterface({
         uri: 'http://localhost:3081/gql',
     })
-} as any);
+});
 
 function replaceTag(content: string) {
     return content
         .replace(/(<? *script)/gi, 'illegalscript')
         .replace(/<figure .*?>.*?<\/figure.*?>/gi, '<image>')
         .replace(/<img .*?>/gi, '<image>')
-        .concat(' ... ')
+        .concat(' ... ');
 }
 
 function convertToDate(tstamp: number) {
 
     const date = new Date(tstamp * 1000);
-    const datevalues = [
-        date.getFullYear(),
-        date.getMonth() + 1,
-        date.getDate(),
-        date.getHours(),
-        date.getMinutes(),
-        date.getSeconds(),
-    ];
-    return datevalues.slice(0, 3).join('/') + ' ' + datevalues.slice(3).join(':');
+    const dateValues = [date.getFullYear(), date.getMonth() + 1, date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds(),];
+    return dateValues.slice(0, 3).join('/') + ' ' + dateValues.slice(3).join(':');
+}
+
+interface ArticleInterface {
+    hash: string;
+    link: string;
+    title: string;
+    publish_at: number;
+    description: string;
 }
 
 interface Props {
-    data: any;
+    data: QueryProps & { articles: ArticleInterface[] };
 }
 
 class ArticleList extends React.Component<Props, object> {
@@ -42,27 +42,28 @@ class ArticleList extends React.Component<Props, object> {
 
     render() {
         const {loading, error, articles} = this.props.data;
-        if (loading) return <section> Loading Articles ... </section>;
-        if (error) return <section> Error Happened ... </section>;
-        return (<section className='articles-container__list-container'>
-            {
-                articles.map((item: any) =>
-                    <section
-                        key={item.hash}
-                        className='articles-container__list-item'
-                        onClick={e => this.openLink(item.link)}
-                    >
-                        <div>
-                            <p>{item.title}</p>
-                            <span>{convertToDate(item.publish_at)}</span>
-                        </div>
-                        <article
-                            className='article-item__content'
-                            dangerouslySetInnerHTML={{__html: replaceTag(item.description)}}
-                        />
-                    </section>)
-            }
-        </section>)
+        if (loading) {
+            return <section> Loading Articles ... </section>;
+        }
+        if (error) {
+            return <section> Error Happened ... </section>;
+        }
+        return (<section className="articles-container__list-container">
+            {articles.map((item) => <section
+                key={item.hash}
+                className="articles-container__list-item"
+                onClick={e => this.openLink(item.link)}
+            >
+                <div>
+                    <p>{item.title}</p>
+                    <span>{convertToDate(item.publish_at)}</span>
+                </div>
+                <article
+                    className="article-item__content"
+                    dangerouslySetInnerHTML={{__html: replaceTag(item.description)}}
+                />
+            </section>)}
+        </section>);
     }
 }
 
@@ -79,18 +80,16 @@ query {
 
 class App extends React.Component {
     render() {
-        return (
-            <ApolloProvider client={client}>
-                <div className='app'>
-                    <div className='app-header'>
-                        <h2>RSS Reader</h2>
-                    </div>
-                    <article className='articles-container'>
-                        <ArticleListWithData/>
-                    </article>
+        return (<ApolloProvider client={client}>
+            <div className="app">
+                <div className="app-header">
+                    <h2>RSS Reader</h2>
                 </div>
-            </ApolloProvider>
-        );
+                <article className="articles-container">
+                    <ArticleListWithData/>
+                </article>
+            </div>
+        </ApolloProvider>);
     }
 }
 

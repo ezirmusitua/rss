0bb04856b7275100a832bed2ba9d83686d956c75
ezirmusitua <jferroal@gmail.com>
2017-12-10 12:23:38 +0800

    Fix parser
    Took 11 hours 19 minutes

--- parser/parser.py
+++ parser/parser.py
@@ -27,7 +27,11 @@ class Article(object):
         result['title'] = content['title']
         result['link'] = content['link']
         result['hash'] = hashlib.md5((content['link'] + content['title']).encode()).hexdigest()
-        result['publish_at'] = datetime.datetime(*content['published_parsed'][:7]).timestamp()
+        published_at_time_tup = content.get('published_parsed', None)
+        if published_at_time_tup:
+            result['publish_at'] = datetime.datetime(*content['published_parsed'][:7]).timestamp()
+        else:
+            result['publish_at'] = -1
         _content, summary = content.get('content', None), content.get('summary', None)
 
         if not _content and not summary:
@@ -52,25 +56,30 @@ async def parse(content):
 async def main(db, interval=60):
     while True:
         sources = await db['feed'].find({}, limit=10).to_list(length=10)
-        done, pending = await asyncio.wait([
-            parse(source['content'])
-            for source in sources
-        ])
-        to_save = list()
-        for future in done:
-            parsed = future.result()
-            for content in parsed.get('entries', list()):
-                to_save.append(Article(content))
-        await asyncio.wait([
-            db['article'].find_one_and_update(article.exists_query, {'$set': article.json}, upsert=True)
-            for article in to_save
-        ])
-        await db['feed'].remove({'_id': {'$in': list(map(lambda x: x['_id'], sources))}})
+        if not sources:
+            print('no feed to parse ... ')
+        else:
+            done, pending = await asyncio.wait([
+                parse(source['content'])
+                for source in sources
+            ])
+            to_save = list()
+            for future in done:
+                parsed = future.result()
+                for content in parsed.get('entries', list()):
+                    to_save.append(Article(content))
+            print('create new articles')
+            await asyncio.wait([
+                db['article'].find_one_and_update(article.exists_query, {'$set': article.json}, upsert=True)
+                for article in to_save
+            ])
+            print('removing parsed feeds ... ')
+            await db['feed'].remove({'_id': {'$in': list(map(lambda x: x['_id'], sources))}})
         await asyncio.sleep(interval)
 
 
 if __name__ == '__main__':
-    db = AsyncIOMotorClient('localhost', 27017)['rss']
+    db = AsyncIOMotorClient('mongodb://localhost:27017/rss')['rss']
     loop = asyncio.get_event_loop()
-    asyncio.ensure_future(main(db, 7200))
+    asyncio.ensure_future(main(db, 3600))
     loop.run_forever()
